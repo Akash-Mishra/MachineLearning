9f862f75637c7d233b454399af4d5853cf8006c9
Akash-Mishra <akash.mishra@innoplexus.com>
2016-12-01 18:01:52 +0530

    example of two class classification problem - visualization complete

--- twoClassClassification.py
+++ twoClassClassification.py
@@ -1,3 +1,11 @@
-
+import mglearn
+import matplotlib.pyplot as plt
 # generate dataset
 X, y = mglearn.datasets.make_forge()
+# plot dataset
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+plt.legend(["class 0", "class 1"], loc=4)
+plt.xlabel("First feature")
+plt.ylabel("Second feature")
+print("X.shape: {}".format(X.shape))
+plt.show()
